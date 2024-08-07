cdb2f68ec4d1ef774faa7a03b5d7858c728bcd83
Chang Liu <liuc223@gmail.com>
2024-08-07 17:44:56 -0400

    Add additional query result util functions

--- query_result.go
+++ query_result.go
@@ -13,12 +13,50 @@ type QueryResult struct {
 }
 
 func (queryResult QueryResult) ToString() string {
-	cstring := C.kuzu_query_result_to_string(&queryResult.CQueryResult)
-	str := C.GoString(cstring)
-	C.free(unsafe.Pointer(cstring))
+	cString := C.kuzu_query_result_to_string(&queryResult.CQueryResult)
+	str := C.GoString(cString)
+	C.free(unsafe.Pointer(cString))
 	return str
 }
 
 func (queryResult QueryResult) Close() {
 	C.kuzu_query_result_destroy(&queryResult.CQueryResult)
+}
+
+func (queryResult QueryResult) ResetIterator() {
+	C.kuzu_query_result_reset_iterator(&queryResult.CQueryResult)
+}
+
+func (queryResult QueryResult) GetColumnNames() []string {
+	numColumns := int64(C.kuzu_query_result_get_num_columns(&queryResult.CQueryResult))
+	columns := make([]string, 0, numColumns)
+	for i := int64(0); i < numColumns; i++ {
+		var outColumn *C.char
+		C.kuzu_query_result_get_column_name(&queryResult.CQueryResult, C.uint64_t(i), &outColumn)
+		defer C.kuzu_destroy_string(outColumn)
+		columns = append(columns, C.GoString(outColumn))
+	}
+	return columns
+}
+
+func (queryResult QueryResult) GetNumberOfRows() uint64 {
+	return uint64(C.kuzu_query_result_get_num_tuples(&queryResult.CQueryResult))
+}
+
+func (queryResult QueryResult) HasNext() bool {
+	return bool(C.kuzu_query_result_has_next(&queryResult.CQueryResult))
+}
+
+func (queryResult QueryResult) GetCompilingTime() float64 {
+	var cQuerySummary C.kuzu_query_summary
+	C.kuzu_query_result_get_query_summary(&queryResult.CQueryResult, &cQuerySummary)
+	defer C.kuzu_query_summary_destroy(&cQuerySummary)
+	return float64(C.kuzu_query_summary_get_compiling_time(&cQuerySummary))
+}
+
+func (queryResult QueryResult) GetExecutionTime() float64 {
+	var cQuerySummary C.kuzu_query_summary
+	C.kuzu_query_result_get_query_summary(&queryResult.CQueryResult, &cQuerySummary)
+	defer C.kuzu_query_summary_destroy(&cQuerySummary)
+	return float64(C.kuzu_query_summary_get_execution_time(&cQuerySummary))
 }
