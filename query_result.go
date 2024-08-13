f0b82a690abfd54cf8d2d5d357fd76197df86846
Chang Liu <liuc223@gmail.com>
2024-08-13 09:45:45 -0400

    Restructure: add FlatTuple struct

--- query_result.go
+++ query_result.go
@@ -5,12 +5,15 @@ package kuzu
 import "C"
 
 import (
+	"fmt"
+	"runtime"
 	"unsafe"
 )
 
 type QueryResult struct {
 	CQueryResult C.kuzu_query_result
 	isClosed     bool
+	columnNames  []string
 }
 
 func (queryResult QueryResult) ToString() string {
@@ -33,6 +36,9 @@ func (queryResult QueryResult) ResetIterator() {
 }
 
 func (queryResult QueryResult) GetColumnNames() []string {
+	if queryResult.columnNames != nil {
+		return queryResult.columnNames
+	}
 	numColumns := int64(C.kuzu_query_result_get_num_columns(&queryResult.CQueryResult))
 	columns := make([]string, 0, numColumns)
 	for i := int64(0); i < numColumns; i++ {
@@ -41,15 +47,32 @@ func (queryResult QueryResult) GetColumnNames() []string {
 		defer C.kuzu_destroy_string(outColumn)
 		columns = append(columns, C.GoString(outColumn))
 	}
+	queryResult.columnNames = columns
 	return columns
 }
 
 func (queryResult QueryResult) GetNumberOfRows() uint64 {
+	if queryResult.columnNames != nil {
+		return uint64(len(queryResult.columnNames))
+	}
 	return uint64(C.kuzu_query_result_get_num_tuples(&queryResult.CQueryResult))
 }
 
 func (queryResult QueryResult) HasNext() bool {
 	return bool(C.kuzu_query_result_has_next(&queryResult.CQueryResult))
+}
+
+func (queryResult QueryResult) Next() (FlatTuple, error) {
+	tuple := FlatTuple{}
+	runtime.SetFinalizer(&tuple, func(tuple *FlatTuple) {
+		tuple.Close()
+	})
+	tuple.queryResult = &queryResult
+	status := C.kuzu_query_result_get_next(&queryResult.CQueryResult, &tuple.CFlatTuple)
+	if status != C.KuzuSuccess {
+		return tuple, fmt.Errorf("failed to get next tuple with status %d", status)
+	}
+	return tuple, nil
 }
 
 func (queryResult QueryResult) GetCompilingTime() float64 {
