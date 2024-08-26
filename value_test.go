5d15d3f4bf0d9cf475a7f0176df388e3cd7022bc
囧囧 <liuc223@gmail.com>
2024-08-26 19:49:08 -0400

    Add tests for primitive types

--- value_test.go
+++ value_test.go
@@ -2,6 +2,7 @@ package kuzu
 
 import (
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -28,6 +29,28 @@ func TestInt64(t *testing.T) {
 	res.Close()
 }
 
+func TestInt32(t *testing.T) {
+	_, conn := SetupTestDatabase(t)
+	res, error := conn.Query("RETURN CAST (170, \"INT32\")")
+	assert.Nil(t, error)
+	assert.True(t, res.HasNext())
+	next, _ := res.Next()
+	value, _ := next.GetValue(0)
+	assert.Equal(t, int32(170), value)
+	res.Close()
+}
+
+func TestInt16(t *testing.T) {
+	_, conn := SetupTestDatabase(t)
+	res, error := conn.Query("RETURN CAST (888, \"INT16\")")
+	assert.Nil(t, error)
+	assert.True(t, res.HasNext())
+	next, _ := res.Next()
+	value, _ := next.GetValue(0)
+	assert.Equal(t, int16(888), value)
+	res.Close()
+}
+
 func TestInt8(t *testing.T) {
 	_, conn := SetupTestDatabase(t)
 	res, error := conn.Query("MATCH (a:person) -[r:studyAt]-> (b:organisation) WHERE r.length = 5 RETURN r.level;")
@@ -39,28 +62,16 @@ func TestInt8(t *testing.T) {
 	res.Close()
 }
 
-func TestUint8(t *testing.T) {
+func TestUint64(t *testing.T) {
 	_, conn := SetupTestDatabase(t)
-	res, error := conn.Query("MATCH (a:person) -[r:studyAt]-> (b:organisation) WHERE r.length = 5 RETURN r.ulevel;")
+	res, error := conn.Query("MATCH (a:person) -[r:studyAt]-> (b:organisation) WHERE r.length = 5 RETURN r.code;")
 	assert.Nil(t, error)
 	assert.True(t, res.HasNext())
 	next, _ := res.Next()
 	value, _ := next.GetValue(0)
-	assert.Equal(t, uint8(250), value)
+	assert.Equal(t, uint64(9223372036854775808), value)
 	res.Close()
 }
-
-func TestUint16(t *testing.T) {
-	_, conn := SetupTestDatabase(t)
-	res, error := conn.Query("MATCH (a:person) -[r:studyAt]-> (b:organisation) WHERE r.length = 5 RETURN r.ulength;")
-	assert.Nil(t, error)
-	assert.True(t, res.HasNext())
-	next, _ := res.Next()
-	value, _ := next.GetValue(0)
-	assert.Equal(t, uint16(33768), value)
-	res.Close()
-}
-
 func TestUint32(t *testing.T) {
 	_, conn := SetupTestDatabase(t)
 	res, error := conn.Query("MATCH (a:person) -[r:studyAt]-> (b:organisation) WHERE r.length = 5 RETURN r.temperature;")
@@ -71,15 +82,24 @@ func TestUint32(t *testing.T) {
 	assert.Equal(t, uint32(32800), value)
 	res.Close()
 }
-
-func TestUint64(t *testing.T) {
+func TestUint16(t *testing.T) {
 	_, conn := SetupTestDatabase(t)
-	res, error := conn.Query("MATCH (a:person) -[r:studyAt]-> (b:organisation) WHERE r.length = 5 RETURN r.code;")
+	res, error := conn.Query("MATCH (a:person) -[r:studyAt]-> (b:organisation) WHERE r.length = 5 RETURN r.ulength;")
 	assert.Nil(t, error)
 	assert.True(t, res.HasNext())
 	next, _ := res.Next()
 	value, _ := next.GetValue(0)
-	assert.Equal(t, uint64(9223372036854775808), value)
+	assert.Equal(t, uint16(33768), value)
+	res.Close()
+}
+func TestUint8(t *testing.T) {
+	_, conn := SetupTestDatabase(t)
+	res, error := conn.Query("MATCH (a:person) -[r:studyAt]-> (b:organisation) WHERE r.length = 5 RETURN r.ulevel;")
+	assert.Nil(t, error)
+	assert.True(t, res.HasNext())
+	next, _ := res.Next()
+	value, _ := next.GetValue(0)
+	assert.Equal(t, uint8(250), value)
 	res.Close()
 }
 
@@ -102,6 +122,17 @@ func TestDouble(t *testing.T) {
 	next, _ := res.Next()
 	value, _ := next.GetValue(0)
 	assert.InDelta(t, float64(5.0), value, floatEpsilon)
+	res.Close()
+}
+
+func TestFloat(t *testing.T) {
+	_, conn := SetupTestDatabase(t)
+	res, error := conn.Query("RETURN CAST (1.75, \"FLOAT\")")
+	assert.Nil(t, error)
+	assert.True(t, res.HasNext())
+	next, _ := res.Next()
+	value, _ := next.GetValue(0)
+	assert.InDelta(t, float32(1.75), value, floatEpsilon)
 	res.Close()
 }
 
@@ -128,4 +159,128 @@ func TestBlob(t *testing.T) {
 	assert.Equal(t, byte(0xCD), value.([]byte)[2])
 	assert.Equal(t, byte(0x1A), value.([]byte)[3])
 	res.Close()
+}
+
+func TestDate(t *testing.T) {
+	_, conn := SetupTestDatabase(t)
+	res, error := conn.Query("RETURN DATE('1985-01-01')")
+	assert.Nil(t, error)
+	assert.True(t, res.HasNext())
+	next, _ := res.Next()
+	value, _ := next.GetValue(0)
+	time := value.(time.Time)
+	assert.Equal(t, 1985, time.Year())
+	assert.Equal(t, 1, int(time.Month()))
+	assert.Equal(t, 1, time.Day())
+	assert.Equal(t, 0, time.Hour())
+	res.Close()
+}
+
+func TestTimestamp(t *testing.T) {
+	_, conn := SetupTestDatabase(t)
+	res, error := conn.Query("RETURN TIMESTAMP('1970-01-01T00:00:00Z')")
+	assert.Nil(t, error)
+	assert.True(t, res.HasNext())
+	next, _ := res.Next()
+	value, _ := next.GetValue(0)
+	time := value.(time.Time)
+	time = time.UTC()
+	assert.Equal(t, 1970, time.Year())
+	assert.Equal(t, 1, int(time.Month()))
+	assert.Equal(t, 1, time.Day())
+	assert.Equal(t, 0, time.Hour())
+	assert.Equal(t, 0, time.Minute())
+	assert.Equal(t, 0, time.Second())
+	res.Close()
+}
+
+func TestTimestampNs(t *testing.T) {
+	_, conn := SetupTestDatabase(t)
+	preparedStatement, error := conn.Prepare("RETURN $1")
+	assert.Nil(t, error)
+	params := map[string]interface{}{
+		"1": time.Date(1970, 1, 1, 0, 0, 0, 1, time.UTC),
+	}
+	res, error := conn.Execute(preparedStatement, params)
+	assert.Nil(t, error)
+	assert.True(t, res.HasNext())
+	next, _ := res.Next()
+	value, _ := next.GetValue(0)
+	time := value.(time.Time)
+	time = time.UTC()
+	assert.Equal(t, 1970, time.Year())
+	assert.Equal(t, 1, int(time.Month()))
+	assert.Equal(t, 1, time.Day())
+	assert.Equal(t, 0, time.Hour())
+	assert.Equal(t, 0, time.Minute())
+	assert.Equal(t, 0, time.Second())
+	assert.Equal(t, 1, time.Nanosecond())
+	res.Close()
+}
+
+func TestTimestampMs(t *testing.T) {
+	_, conn := SetupTestDatabase(t)
+	preparedStatement, error := conn.Prepare("RETURN CAST ($1, \"TIMESTAMP_MS\")")
+	assert.Nil(t, error)
+	inputTime, error := time.Parse(time.RFC3339, "2024-08-29T10:03:05Z")
+	// Add 3 milliseconds
+	duration, err := time.ParseDuration("3ms")
+	if err != nil {
+		t.Fatal(err)
+	}
+	inputTime = inputTime.Add(duration)
+	assert.Nil(t, error)
+	params := map[string]interface{}{
+		"1": inputTime,
+	}
+	res, error := conn.Execute(preparedStatement, params)
+	assert.Nil(t, error)
+	assert.True(t, res.HasNext())
+	next, _ := res.Next()
+	value, _ := next.GetValue(0)
+	assert.Equal(t, inputTime.Local(), value)
+}
+
+func TestTimestampSec(t *testing.T) {
+	_, conn := SetupTestDatabase(t)
+	preparedStatement, error := conn.Prepare("RETURN CAST ($1, \"TIMESTAMP_SEC\")")
+	assert.Nil(t, error)
+	inputTime, error := time.Parse(time.RFC3339, "2024-08-29T10:03:05Z")
+	assert.Nil(t, error)
+	params := map[string]interface{}{
+		"1": inputTime,
+	}
+	res, error := conn.Execute(preparedStatement, params)
+	assert.Nil(t, error)
+	assert.True(t, res.HasNext())
+	next, _ := res.Next()
+	value, _ := next.GetValue(0)
+	assert.Equal(t, inputTime.Local(), value)
+}
+
+func TestTimestampTz(t *testing.T) {
+	_, conn := SetupTestDatabase(t)
+	preparedStatement, error := conn.Prepare("RETURN CAST ($1, \"TIMESTAMP_TZ\")")
+	assert.Nil(t, error)
+	inputTime, error := time.Parse(time.RFC3339, "2024-08-29T10:03:05Z")
+	assert.Nil(t, error)
+	params := map[string]interface{}{
+		"1": inputTime,
+	}
+	res, error := conn.Execute(preparedStatement, params)
+	assert.Nil(t, error)
+	assert.True(t, res.HasNext())
+	next, _ := res.Next()
+	value, _ := next.GetValue(0)
+	assert.Equal(t, inputTime.Local(), value)
+}
+
+func TestInterval(t *testing.T) {
+	_, conn := SetupTestDatabase(t)
+	res, error := conn.Query("RETURN INTERVAL(\"3 days\");")
+	assert.Nil(t, error)
+	assert.True(t, res.HasNext())
+	next, _ := res.Next()
+	value, _ := next.GetValue(0)
+	assert.Equal(t, time.Duration(3*24*time.Hour), value)
 }
