de54ab17c187527f4f8be83ecd71469003c94e70
Chang Liu <liuc223@gmail.com>
2024-08-26 15:21:42 -0400

    Add additional connection tests

--- connection_test.go
+++ connection_test.go
@@ -1,7 +1,9 @@
 package kuzu
 
 import (
+	"sync"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -37,5 +39,127 @@ func TestSetMaxNumThreads(t *testing.T) {
 	conn, _ := OpenConnection(db)
 	conn.SetMaxNumThreads(3)
 	assert.Equal(t, uint64(3), conn.GetMaxNumThreads())
+	conn.Close()
+}
+
+func TestInterrupt(t *testing.T) {
+	query := "MATCH (a:person)-[k:knows*1..28]->(b:person) RETURN COUNT(*);"
+	db, _ := SetupTestDatabase(t)
+	conn, _ := OpenConnection(db)
+	var err error
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		_, err = conn.Query(query)
+		wg.Done()
+	}()
+	time.Sleep(100 * time.Millisecond)
+	conn.Interrupt()
+	wg.Wait()
+	assert.NotNil(t, err)
+	assert.Equal(t, err.Error(), "Interrupted.")
+	conn.Close()
+}
+
+func TestSetTimeout(t *testing.T) {
+	query := "MATCH (a:person)-[k:knows*1..28]->(b:person) RETURN COUNT(*);"
+	db, _ := SetupTestDatabase(t)
+	conn, _ := OpenConnection(db)
+	conn.SetTimeout(100)
+	_, err := conn.Query(query)
+	assert.NotNil(t, err)
+	assert.Equal(t, err.Error(), "Interrupted.")
+	conn.Close()
+}
+
+func TestQuery(t *testing.T) {
+	query := "RETURN CAST(1, \"INT64\");"
+	db, _ := SetupTestDatabase(t)
+	conn, _ := OpenConnection(db)
+	result, err := conn.Query(query)
+	assert.Nil(t, err)
+	assert.NotNil(t, result)
+	assert.NotNil(t, result.cQueryResult)
+	assert.True(t, result.HasNext())
+	flatTuple, err := result.Next()
+	assert.Nil(t, err)
+	assert.NotNil(t, flatTuple)
+	slice, err := flatTuple.GetAsSlice()
+	assert.Nil(t, err)
+	assert.Equal(t, int64(1), slice[0])
+	result.Close()
+	conn.Close()
+}
+
+func TestQueryError(t *testing.T) {
+	query := "RETURN a;"
+	db, _ := SetupTestDatabase(t)
+	conn, _ := OpenConnection(db)
+	_, err := conn.Query(query)
+	assert.NotNil(t, err)
+	assert.Contains(t, err.Error(), "Variable a is not in scope.")
+	conn.Close()
+}
+
+func TestPrepare(t *testing.T) {
+	query := "RETURN $a;"
+	db, _ := SetupTestDatabase(t)
+	conn, _ := OpenConnection(db)
+	stmt, err := conn.Prepare(query)
+	assert.Nil(t, err)
+	assert.NotNil(t, stmt)
+	assert.NotNil(t, stmt.cPreparedStatement)
+	stmt.Close()
+	assert.True(t, stmt.isClosed)
+	// Double close should not panic
+	stmt.Close()
+	assert.True(t, stmt.isClosed)
+	conn.Close()
+}
+
+func TestPrepareError(t *testing.T) {
+	query := "MATCH RETURN $a;"
+	db, _ := SetupTestDatabase(t)
+	conn, _ := OpenConnection(db)
+	stmt, err := conn.Prepare(query)
+	assert.NotNil(t, err)
+	assert.Contains(t, err.Error(), "Parser exception")
+	stmt.Close()
+	conn.Close()
+}
+
+func TestExecute(t *testing.T) {
+	query := "RETURN $a;"
+	db, _ := SetupTestDatabase(t)
+	conn, _ := OpenConnection(db)
+	stmt, _ := conn.Prepare(query)
+	args := map[string]any{"a": int64(1)}
+	result, err := conn.Execute(stmt, args)
+	assert.Nil(t, err)
+	assert.NotNil(t, result)
+	assert.NotNil(t, result.cQueryResult)
+	assert.True(t, result.HasNext())
+	flatTuple, err := result.Next()
+	assert.Nil(t, err)
+	assert.NotNil(t, flatTuple)
+	slice, err := flatTuple.GetAsSlice()
+	assert.Nil(t, err)
+	assert.Equal(t, int64(1), slice[0])
+	result.Close()
+	stmt.Close()
+	conn.Close()
+}
+
+func TestExecuteError(t *testing.T) {
+	query := "RETURN $a;"
+	db, _ := SetupTestDatabase(t)
+	conn, _ := OpenConnection(db)
+	stmt, _ := conn.Prepare(query)
+	args := map[string]any{"b": int64(1)}
+	result, err := conn.Execute(stmt, args)
+	assert.NotNil(t, err)
+	assert.Contains(t, err.Error(), "Parameter b not found")
+	result.Close()
+	stmt.Close()
 	conn.Close()
 }
