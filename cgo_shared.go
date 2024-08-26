1a993af1317d6feecd88ac89f65242fc0c3a93ac
Chang Liu <liuc223@gmail.com>
2024-08-25 22:11:41 -0400

    Add test datasets and remove linking to cpp on Linux

--- cgo_shared.go
+++ cgo_shared.go
@@ -2,8 +2,8 @@ package kuzu
 
 /*
 #cgo darwin LDFLAGS: -lc++ -L${SRCDIR}/lib/dynamic/darwin -lkuzu -Wl,-rpath,${SRCDIR}/lib/dynamic/darwin
-#cgo linux,amd64 LDFLAGS: -lc++ -L${SRCDIR}/lib/dynamic/linux-amd64 -lkuzu -Wl,-rpath,${SRCDIR}/lib/dynamic/linux-amd64
-#cgo linux,arm64 LDFLAGS: -lc++ -L${SRCDIR}/lib/dynamic/linux-arm64 -lkuzu -Wl,-rpath,${SRCDIR}/lib/dynamic/linux-arm64
+#cgo linux,amd64 LDFLAGS: -L${SRCDIR}/lib/dynamic/linux-amd64 -lkuzu -Wl,-rpath,${SRCDIR}/lib/dynamic/linux-amd64
+#cgo linux,arm64 LDFLAGS: -L${SRCDIR}/lib/dynamic/linux-arm64 -lkuzu -Wl,-rpath,${SRCDIR}/lib/dynamic/linux-arm64
 #include "kuzu.h"
 */
 import "C"
