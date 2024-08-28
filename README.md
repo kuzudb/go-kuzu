70b30e990044146f2a616d86516b43a9740fc86c
囧囧 <liuc223@gmail.com>
2024-08-28 10:20:01 -0400

    Update README and example (#20)

---
+++ README.md
@@ -0,0 +1,37 @@
+# go-kuzu
+[![Go Reference](https://pkg.go.dev/badge/github.com/kuzudb/go-kuzu.svg)](https://pkg.go.dev/github.com/kuzudb/go-kuzu)
+[![CI](https://github.com/kuzudb/go-kuzu/actions/workflows/go.yml/badge.svg)](https://github.com/kuzudb/go-kuzu/actions/workflows/go.yml)
+[![Go Report Card](https://goreportcard.com/badge/github.com/kuzudb/go-kuzu)](https://goreportcard.com/report/github.com/kuzudb/go-kuzu)
+[![License](https://img.shields.io/github/license/kuzudb/go-kuzu)](LICENSE)
+
+Official Go language binding for [Kùzu](https://github.com/kuzudb/kuzu). Kùzu an embeddable property graph database management system built for query speed and scalability. For more information, please visit the [Kùzu GitHub repository](https://github.com/kuzudb/kuzu) or the [Kùzu website](https://kuzudb.com).
+
+## Installation
+
+```bash
+go get github.com/kuzudb/go-kuzu
+```
+
+## Get started
+An example project is available in the [example](example) directory.
+
+To run the example project, you can use the following command:
+
+```bash
+cd example
+go run main.go
+```
+
+## Docs
+The full documentation is available at [pkg.go.dev](https://pkg.go.dev/github.com/kuzudb/go-kuzu).
+
+## Tests
+To run the tests, you can use the following command:
+
+```bash
+go test -v
+```
+
+## Contributing
+We welcome contributions to go-kuzu. By contributing to go-kuzu, you agree that your contributions will be licensed under the [MIT License](LICENSE). Please read the [contributing guide](CONTRIBUTING.md) for more information.
+
---
+++ example/main.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/kuzudb/go-kuzu"
+)
+
+func main() {
+	dbPath := "example_db"
+	os.RemoveAll(dbPath)
+
+	// Open a database with default system configuration.
+	systemConfig := kuzu.DefaultSystemConfig()
+	systemConfig.BufferPoolSize = 1024 * 1024 * 1024
+	db, err := kuzu.OpenDatabase(dbPath, systemConfig)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	// Open a connection to the database.
+	conn, err := kuzu.OpenConnection(db)
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close()
+
+	// Set up the schema and load data.
+	queries := []string{
+		"CREATE NODE TABLE User(name STRING, age INT64, PRIMARY KEY (name))",
+		"CREATE NODE TABLE City(name STRING, population INT64, PRIMARY KEY (name))",
+		"CREATE REL TABLE Follows(FROM User TO User, since INT64)",
+		"CREATE REL TABLE LivesIn(FROM User TO City)",
+		"COPY User FROM \"../dataset/demo-db/user.csv\"",
+		"COPY City FROM \"../dataset/demo-db/city.csv\"",
+		"COPY Follows FROM \"../dataset/demo-db/follows.csv\"",
+		"COPY LivesIn FROM \"../dataset/demo-db/lives-in.csv\"",
+	}
+	for _, query := range queries {
+		fmt.Println("Executing query:", query)
+		queryResult, err := conn.Query(query)
+		if err != nil {
+			panic(err)
+		}
+		defer queryResult.Close()
+	}
+
+	query := "MATCH (a:User)-[e:Follows]->(b:User) RETURN a.name, e.since, b.name"
+	println("Executing query:", query)
+	// Execute a query and print the result.
+	result, err := conn.Query(query)
+	if err != nil {
+		panic(err)
+	}
+	defer result.Close()
+	for result.HasNext() {
+		tuple, err := result.Next()
+		if err != nil {
+			panic(err)
+		}
+		defer tuple.Close()
+		// The result is a tuple, which can be converted to a slice or a map.
+		slice, err := tuple.GetAsSlice()
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(slice)
+		m, err := tuple.GetAsMap()
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(m)
+	}
+
+	// Execute a query with parameters.
+	query = "MATCH (a:User) WHERE a.name = $name RETURN a.age"
+	println("Executing query:", query)
+	preparedStatement, err := conn.Prepare(query)
+	if err != nil {
+		panic(err)
+	}
+	defer preparedStatement.Close()
+	args := map[string]interface{}{"name": "Adam"}
+	result, err = conn.Execute(preparedStatement, args)
+	if err != nil {
+		panic(err)
+	}
+	defer result.Close()
+	for result.HasNext() {
+		tuple, err := result.Next()
+		if err != nil {
+			panic(err)
+		}
+		defer tuple.Close()
+		// The tuple can also be converted to a string.
+		fmt.Print(tuple.GetAsString())
+	}
+
+	fmt.Println("All queries executed successfully.")
+}
