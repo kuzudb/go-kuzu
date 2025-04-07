58d01926aa4d42e36eb7b6e9edca9acd5fdfbbb1
Chang Liu <liuc223@gmail.com>
2025-04-06 22:24:57 -0400

    x

--- cgo_shared.go
+++ cgo_shared.go
@@ -4,7 +4,7 @@ package kuzu
 #cgo darwin LDFLAGS: -lc++ -L${SRCDIR}/lib/dynamic/darwin -lkuzu -Wl,-rpath,${SRCDIR}/lib/dynamic/darwin
 #cgo linux,amd64 LDFLAGS: -L${SRCDIR}/lib/dynamic/linux-amd64 -lkuzu -Wl,-rpath,${SRCDIR}/lib/dynamic/linux-amd64
 #cgo linux,arm64 LDFLAGS: -L${SRCDIR}/lib/dynamic/linux-arm64 -lkuzu -Wl,-rpath,${SRCDIR}/lib/dynamic/linux-arm64
-#cgo windows LDFLAGS: -L${SRCDIR}/lib/dynamic/windows -lkuzu -Wl,-rpath,${SRCDIR}/lib/dynamic/windows
+#cgo windows LDFLAGS: -L${SRCDIR}/lib/dynamic/windows -lkuzu_shared
 #include "kuzu.h"
 */
 import "C"
